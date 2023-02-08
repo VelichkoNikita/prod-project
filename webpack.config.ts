880985974573be4a9e293e6c74f1bd8c3567bc34
VelichkoNikita <n.velichko@smartplayer.org>
2023-02-08 10:09:33 +0300

    FSD remake, add babel and other

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,18 +1,22 @@
-import webpack from 'webpack';
+import webpack from 'webpack'
 import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
-import {BuildPaths, BuildEnv} from "./config/build/types/config";
+import {BuildEnv, BuildPaths} from "./config/build/types/config";
 import path from "path";
 
-export default (env: BuildEnv)=>{
+
+export default (env: BuildEnv) => {
+
     const paths: BuildPaths = {
         entry: path.resolve(__dirname, 'src', 'index.tsx'),
         build: path.resolve(__dirname, 'build'),
         html: path.resolve(__dirname, 'public', 'index.html'),
+        src: path.resolve(__dirname, 'src'),
     }
 
     const mode = env.mode || 'development';
-    const isDev = mode === 'development';
     const PORT = env.port || 3000;
+
+    const isDev = mode === 'development';
 
     const config: webpack.Configuration = buildWebpackConfig({
         mode,
@@ -20,5 +24,6 @@ export default (env: BuildEnv)=>{
         isDev,
         port: PORT,
     })
+
     return config
 };
