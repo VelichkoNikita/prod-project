149ff8215455ef02de728e5a8b4f63da1246e714
VelichkoNikita <n.velichko@smartplayer.org>
2023-02-25 18:39:43 +0300

    add pre auth logic

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -1,20 +1,19 @@
 import { configureStore, ReducersMapObject } from '@reduxjs/toolkit';
 import { counterReducer } from 'entities/Counter';
 import { userReducer } from 'entities/User';
+import { loginReducer } from 'features/AuthByUsername';
 import { StateSchema } from './StateSchema';
 
 export function createReduxStore(initialState?: StateSchema) {
-    const rootReducer: ReducersMapObject<StateSchema> = {
+    const rootReducers: ReducersMapObject<StateSchema> = {
         counter: counterReducer,
         user: userReducer,
+        loginForm: loginReducer,
     };
 
     return configureStore<StateSchema>({
-        reducer: rootReducer,
+        reducer: rootReducers,
         devTools: __IS_DEV__,
         preloadedState: initialState,
     });
 }
-
-// export type RootState = ReturnType<typeof store.getState>
-// export type AppDispatch = typeof store.dispatch
