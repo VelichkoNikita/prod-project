ddcb3abe0ceb2e1385cb27dc7b85bb2fd2dbbe13
VelichkoNikita <n.velichko@smartplayer.org>
2023-02-28 11:49:53 +0300

    add profile, memo optimization

---
+++ src/shared/const/common.ts
@@ -0,0 +1,13 @@
+export interface Currency {
+    'RUB' :'RUB',
+    'EUR': 'EUR',
+    'USD': 'USD',
+}
+
+export interface Country {
+    Russia : 'Russia',
+    Belarus : 'Belarus',
+    Ukraine : 'Ukraine',
+    Kazakhstan : 'Kazakhstan',
+    Armenia : 'Armenia',
+}
