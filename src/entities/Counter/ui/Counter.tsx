625799ee1bdcb6a8587e516374d1d1b87e3d790a
VelichkoNikita <n.velichko@smartplayer.org>
2023-03-08 15:51:09 +0300

    add config

--- src/entities/Counter/ui/Counter.tsx
+++ src/entities/Counter/ui/Counter.tsx
@@ -6,6 +6,7 @@ import { getCounterValue } from '../model/selectors/getCounterValue/getCounterVa
 
 export const Counter = () => {
     const dispatch = useDispatch();
+
     const counterValue = useSelector(getCounterValue);
     const { t } = useTranslation();
 
