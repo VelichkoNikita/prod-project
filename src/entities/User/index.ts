11a760b8d8571a4e6769a6176c4f7de4e494bd02
VelichkoNikita <n.velichko@smartplayer.org>
2023-04-15 14:52:33 +0300

    add private route with required roles

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -6,6 +6,8 @@ export {
     getUserInited,
 } from './model/selectors/getUserInited/getUserInited';
 
+export { isUserAdmin, isUserManager, getUserRoles } from './model/selectors/roleSelectors';
+
 export {
     userReducer,
     userActions,
@@ -13,4 +15,5 @@ export {
 export {
     UserSchema,
     User,
-} from './model/types/userTypes';
+    UserRole,
+} from './model/types/user';
