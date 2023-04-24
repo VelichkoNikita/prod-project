b94db4f336ff7bf678a9289d7d7c3f6b21b1b601
VelichkoNikita <n.velichko@smartplayer.org>
2023-04-24 09:30:15 +0300

    add update es lint plugin, feature in project it

--- .eslintrc.js
+++ .eslintrc.js
@@ -62,7 +62,7 @@ module.exports = {
         'no-undef': 'off',
         'react/no-array-index-key': 'off',
         'arrow-body-style': 'off',
-        'vel-plugin/path-checker': 'error',
+        'vel-plugin/path-checker': ['error', { alias: '@' }],
     },
     globals: {
         __IS_DEV__: true,
